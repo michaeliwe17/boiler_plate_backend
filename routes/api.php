3f813651402fd6cd65307fb2fb5c20508b2fb8a6
Michaeliwe <michaeliwe98@gmail.com>
2023-11-03 16:58:39 +0700

    jwt

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+});
+
+Route::group(['middleware' => ['auth:api']], function(){
+    Route::get('/', function () {
+        return ["message" => "bisaaa"];
+    });
+});
+
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return ["message" => "Welcome"];
 });
+Route::get('/login', function () {
+    return ["message" => "Login dulu bosqu"];
+})->name('login');
